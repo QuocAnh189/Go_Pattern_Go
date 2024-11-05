acf5f9d898116af16ee15f98e7a0f3ecc277e38e
QuocAnh189 <anhquoc18092003@gmail.com>
2024-11-05 11:34:50 +0700

    add builder pattern

---
+++ creational_patterns/builder/README.md
@@ -0,0 +1,3 @@
+### Builder
+
+Tách biệt quá trình xây dựng một đối tượng phức tạp khỏi biểu diễn của nó, cho phép tạo ra các biểu diễn khác nhau. Mẫu này hữu ích khi tạo các đối tượng phức tạp có nhiều thành phần tùy chọn
---
+++ creational_patterns/builder/builder.go
@@ -0,0 +1,53 @@
+package builder
+
+import "fmt"
+
+type House struct {
+	WindowType string
+	DoorType   string
+	Floor      int
+}
+
+type HouseBuilder struct {
+	windowType string
+	doorType   string
+	floor      int
+}
+
+func NewHouseBuilder() *HouseBuilder {
+	return &HouseBuilder{}
+}
+
+func (b *HouseBuilder) SetWindowType(windowType string) *HouseBuilder {
+	b.windowType = windowType
+	return b
+}
+
+func (b *HouseBuilder) SetDoorType(doorType string) *HouseBuilder {
+	b.doorType = doorType
+	return b
+}
+
+func (b *HouseBuilder) SetFloor(floor int) *HouseBuilder {
+	b.floor = floor
+	return b
+}
+
+func (b *HouseBuilder) Build() *House {
+	return &House{
+		WindowType: b.windowType,
+		DoorType:   b.doorType,
+		Floor:      b.floor,
+	}
+}
+
+
+func main() {
+	house := NewHouseBuilder().
+		SetWindowType("Sliding Window").
+		SetDoorType("Wooden Door").
+		SetFloor(2).
+		Build()
+
+	fmt.Printf("House: %+v\n", house)
+}
